b8fa9f5d0c50a9a6514766ffd72bbe0ebd8b072e
kiki <kiki>
2022-08-29 18:00:20 -0400

    add a subnet resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "kiki_vpc" {
     "name" = "dev"
   }
 }
+
+resource "aws_subnet" "kiki_public_subnet" {
+    vpc_id = aws_vpc.kiki_vpc.id
+    cidr_block = "10.0.0.0/17"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1b"
+
+       tags = {
+          name = "dev-public"
+}
+}
