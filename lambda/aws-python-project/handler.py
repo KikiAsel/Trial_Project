c5b3323f8834b405671833ce983f3a60c1f26dea
kiki <kiki>
2022-08-31 07:35:50 -0400

    updated lambda function

--- lambda/aws-python-project/handler.py
+++ lambda/aws-python-project/handler.py
@@ -1,5 +1,7 @@
 import json
-
+import ssl
+import socket
+import datetime
 
 def hello(event, context):
     body = {
@@ -8,3 +10,22 @@ def hello(event, context):
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
+
+def ssl_expiry_datetime(host, port=443):
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
+    context = ssl.create_default_context()
+    conn = context.wrap_socket(
+        socket.socket(socket.AF_INET),
+        server_hostname=host,
+    )
+    # 3 second timeout because Lambda has runtime limitations
+    conn.settimeout(3.0)
+    conn.connect((host, port))
+    ssl_info = conn.getpeercert()
+    print(ssl_info)
+    # parse the string from the certificate into a Python datetime object
+    res = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+    return res
+
+print(ssl_expiry_datetime("google.com"))
